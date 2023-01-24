66d0aefa600a944d48724f2a67588b66c61a3509
Silvabru5 <bruno.silva9009@hotmail.com>
2023-01-24 10:24:07 -0500

    added address to student1

--- src/student1/Student1.java
+++ src/student1/Student1.java
@@ -6,6 +6,7 @@ package student1;
 public class Student1 {
     private String studentId;
     private String studentName;
+    private String address;
 
     public Student1(String studentId, String studentName) {
         this.studentId = studentId;
@@ -27,5 +28,15 @@ public class Student1 {
     public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
     
 }
